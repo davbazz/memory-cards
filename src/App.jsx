586c4b8a4ff893c1cf1bb63f2f3b0afb6b7561e0
David <callmelmao@gmail.com>
2022-10-25 21:36:17 +0100

    add css styles to initial page after checking it on my phone

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import InitialPage from './Components/InitialPage'
 function App() {
   
   return (
-    <div className="App p-8 min-h-screen bg-blue-100">
+    <div className="App sm:p-8 p-4 min-h-screen bg-blue-100">
       <InitialPage />
     </div>
   )
