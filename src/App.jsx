4c74598aac7f960faa54c4466a9f0ad738029659
David <callmelmao@gmail.com>
2022-10-25 21:05:00 +0100

    finished initial page responsive layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import InitialPage from './Components/InitialPage'
+
+function App() {
+  
+  return (
+    <div className="App p-8 min-h-screen bg-blue-100">
+      <InitialPage />
+    </div>
+  )
+}
+
+export default App
