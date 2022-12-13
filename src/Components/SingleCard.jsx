5b2d250a8d0f6eba84ef2565ac8e42b90bec34c2
David <callmelmao@gmail.com>
2022-12-13 20:50:44 +0000

    chnaged border

--- src/Components/SingleCard.jsx
+++ src/Components/SingleCard.jsx
@@ -16,12 +16,12 @@ function SingleCard({ card, handleChoice, flipped, disabled, cover }) {
                     <img
                         src={card.src} 
                         alt="card front" 
-                        className="sm:border-2 border-[1px] border-lightBlue dark:border-lightOrange rounded-2xl w-full h-full duration-0"
+                        className=" rounded-2xl w-full h-full duration-0"
                     />
                 </div>
                 <div 
                     onClick={handleClick}
-                    className="back relative text-center rounded-2xl w-full h-full bg-[#fcda72] dark:bg-[#7ea6a1] shadow-[4px_4px_#AED6D1] active:shadow-none dark:shadow-[4px_4px] dark:shadow-lightOrange duration-0">
+                    className="back relative text-center rounded-2xl w-full h-full bg-[#fcda72] dark:bg-[#7ea6a1] sm:shadow-[4px_4px_#AED6D1] shadow-[2px_2px_#AED6D1] active:shadow-none sm:dark:shadow-[4px_4px] dark:shadow-[2px_2px] dark:shadow-lightOrange sm:dark:shadow-lightOrange duration-0">
                     <img 
                         src={cover}
                         className="img-cover 2xl:w-[30%] 2xl:h-[50%] xl:w-[36%] xl:h-[54%] lg:w-[40%] lg:h-[50%] md:w-[46%] md-[50%] sm:w-[50%] sm:h-[50%] w-[60%] h-[60%]"
